3096c29e40c87df6afb506ba89470141c5ac8a71
Juliano <sjulianoluiz@gmail.com>
2021-05-01 16:42:10 -0300

    Initial setup.
    Configure typescript.
    Configure module-alias.

---
+++ src/util/module-alias.ts
@@ -0,0 +1,12 @@
+/**
+ * This file is used to alias the module to convert '@src' to '/src' and '@test' to '/test'.
+ */
+import * as path from 'path';
+import moduleAlias from 'module-alias';
+
+const files = path.resolve(__dirname, '../..');
+
+moduleAlias.addAliases({
+    '@src': path.join(files, 'src'),
+    '@test': path.join(files, 'test')
+});
