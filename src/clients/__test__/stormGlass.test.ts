c1f1c77d30cbfaaabd0d27dedb52094d801f7639
Juliano <sjulianoluiz@gmail.com>
2021-05-05 23:01:15 -0300

    Add type inference for mockedAxios in the test case.

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,15 +6,15 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  
   it('should return the normalized forecast from the forecast service', async () => {
     const latitude = 321321;
     const longitude = 4646;
 
-    axios.get = jest.fn().mockResolvedValue({
-      data: stormGlassWeather3HoursFixture
-    });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(latitude, longitude);
     
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
