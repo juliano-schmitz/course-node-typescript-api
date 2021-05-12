8f737c22758f7c25d4b7badcfe6831076990caa5
Juliano <sjulianoluiz@gmail.com>
2021-05-12 07:57:22 -0300

    Configure database.
    Add mongoose library.
    Add default configuration to the database.
    Add jest configuration to the database.

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import { urlencoded, json, Application } from 'express';
 import { ForecastController } from './controllers/forecast';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
   private setupExpress(): void {
@@ -26,6 +28,15 @@ export class SetupServer extends Server {
     const forecastController = new ForecastController();
 
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+  // TODO: refatorar para fechar toda aplicação, alẽm do banco de dados
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
