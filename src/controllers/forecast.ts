104601360bc748fae367760bbf9fb101f3609928
Juliano <sjulianoluiz@gmail.com>
2021-05-13 23:41:53 -0300

    Add the test case when an error occurs.
    Add the test case that returns 500 to forecast controller.
    Forecasting errors should not be passed on to the user.

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -9,8 +9,12 @@ const forecastService = new Forecast();
 export class ForecastController {
   @Get('')
   public async getForecastForLoggedUser(_: Request, res: Response): Promise<void> { // TODO: refatorar para enviar os dados não estáticos
-    const beaches = await Beach.find({});
-    const forecastData = await forecastService.processForecastForBeaches(beaches);
-    res.status(200).send(forecastData);
+    try {
+      const beaches = await Beach.find({});
+      const forecastData = await forecastService.processForecastForBeaches(beaches);
+      res.status(200).send(forecastData);
+    } catch(error) {
+      res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
